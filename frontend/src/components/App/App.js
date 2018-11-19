3a8d54843323283e042a4f59a56e620e8d3a530b
Glauber Costa Vila Verde <glauber.vila.verde@gmail.com>
2018-11-19 17:01:12 -0200

    Created method to change the images.

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -5,15 +5,51 @@ import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import VisiomaticPanel from '../Visiomatic/Panel'
 import Header from '../Header/Header';
+import {Toolbar} from 'primereact/toolbar';
+import { Button } from 'primereact/button';
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      image: 'v159494-fg-focalplane-summary.ptif',
+      images: [
+        'v159494-fg-focalplane-summary.ptif',
+        'v12456-fy-focalplane-summary.ptif',
+        'v166969-fy-focalplane-summary.ptif',
+        'v161984-fy-focalplane-summary.ptif',
+        'v174549-fi-focalplane-summary.ptif',
+        'v167899-fy-focalplane-summary.ptif',
+        'v174602-fi-focalplane-summary.ptif',
+        'v32678-fz-focalplane-summary.ptif',
+        'v37648-fy-focalplane-summary.ptif',
+        'v8027-fz-focalplane-summary.ptif',
+      ]
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.onChangeImage, 20000);
+  }
+
+  componentWillUnmount() {
+    this.interval.clearInterval();
+  }
+
+  onChangeImage = () => {
+    const id = Math.floor(Math.random() * 10);
+    this.setState({image: this.state.images[id]})
+  }
+
   render() {
- 
-    return (
+     return (
       <div className="layout-wrapper">
         <Header />
         <div className="layout-content">
-          <VisiomaticPanel />
+          <VisiomaticPanel image={this.state.image}/>
         </div>
       </div>
     );
